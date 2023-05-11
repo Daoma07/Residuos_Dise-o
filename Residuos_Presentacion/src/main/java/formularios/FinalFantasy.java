331a43c653e772e1de4e0a099411b0f38bd72a98
DESKTOP-VV22NGB\Jairi <jairo.rodriguez213248@potros.itson.edu.mx>
2023-05-10 23:35:58 -0700

    Prueba

---
+++ Residuos_Presentacion/src/main/java/formularios/FinalFantasy.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formularios;
+
+/**
+ *
+ * @author Jairo G. Rodriguez Hernandez 00000213248
+ */
+public class FinalFantasy {
+    
+}
